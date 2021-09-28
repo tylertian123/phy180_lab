a02a963c63e73709f0e9a033c0bce4eb74ca43d2
Tyler Tian <tylertian123@gmail.com>
2021-09-28 03:16:32 -0400

    Update fit.py

--- fit.py
+++ fit.py
@@ -22,6 +22,17 @@ function).
 
 The program will print the values of A, tau, T and phi along with their standard deviations.
 The standard deviations can be used as uncertainties.
+
+REMEMBER TO SET YOUR INITIAL GUESSES ON LINE 39!!
+If you don't set these correctly according to your data, then the program might not be able
+to compute the best-fit curve. To get an estimate, try running with DO_FIT = False (line 39),
+which turns off the fitting and just plots the points. With your data points you should be
+able to estimate your amplitude, period, and any phase shift. I have not figured out a way
+to estimate tau, so leaving it at 20-100 should be good.
+
+Note: Make sure you give it a lot of data. Observations show that if you only give it a few
+periods, the decay may be negligible and as a result the estimated tau may be very large and
+completely unrealistic (e.g. > 10^6).
 """
 
 import functools
@@ -32,19 +43,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
 
+DO_FIT = True
+# Your initial guess of (a, tau, T, phi)
+INIT_GUESS = (1.1, 20, 1.1, 0)
+
 # Function used to fit
 # First variable is the x-data, and the rest are the parameters we want to determine
 def fit_func(t: float, a: float, tau: float, period: float, phi: float) -> float:
     return a * np.exp(-t / tau) * np.cos(2 * np.pi * t / period + phi)
 
 def fit(x_data, y_data) -> Tuple[Tuple[float, float, float, float], Tuple[float, float, float, float]]:
-    # Your initial guess of (a, tau, T, phi)
-    # TODO: Prompt this instead
-    init_guess = (0.6, 15, 1.0, 0)
-
     # popt: least-squares optimized values for the parameters
     # pcov: covariance matrix
-    popt, pcov = optimize.curve_fit(fit_func, x_data, y_data, p0=init_guess)
+    popt, pcov = optimize.curve_fit(fit_func, x_data, y_data, p0=INIT_GUESS)
 
     a = popt[0]
     tau = popt[1]
@@ -97,50 +108,55 @@ if __name__ == "__main__":
         time -= init_time
         x_data.append(time)
         y_data.append(angle)
-    x_data = np.array(x_data)
-    y_data = np.array(y_data)
+    x_data = np.array(x_data, dtype=np.float128)
+    y_data = np.array(y_data, dtype=np.float128)
 
-    (a, tau, period, phi), (stdev_a, stdev_tau, stdev_period, stdev_phi) = fit(x_data, y_data)
-    bestfit = functools.partial(fit_func, a=a, tau=tau, period=period, phi=phi)
+    if DO_FIT:
+        (a, tau, period, phi), (stdev_a, stdev_tau, stdev_period, stdev_phi) = fit(x_data, y_data)
+        bestfit = functools.partial(fit_func, a=a, tau=tau, period=period, phi=phi)
 
     # Plot best fit curve
     start, stop = min(x_data), max(x_data)
-    # Plot 1000 points for the best fit curve; this can be changed
-    x_vals = np.arange(start, stop, (stop - start) / 1000)
-    y_vals = bestfit(x_vals)
+    if DO_FIT:
+        # Plot 1000 points for the best fit curve; this can be changed
+        x_vals = np.arange(start, stop, (stop - start) / 1000)
+        y_vals = bestfit(x_vals)
 
     fig, (ax1, ax2) = plt.subplots(2, 1)
     # hspace is horizontal space between the graphs
     fig.subplots_adjust(hspace=0.6)
 
+    if DO_FIT:
+        # Plot the best fit curve on top of the data points as a line
+        ax1.plot(x_vals, y_vals, "r", label="Best Fit Curve")
     # Plot the data
-    ax1.scatter(x_data, y_data, label="Collected Data")
+    ax1.scatter(x_data, y_data, label="Collected Data", s=4)
     #ax1.errorbar(xdata, ydata, yerr=yerror, xerr=xerror, fmt=".")
-    # Plot the best fit curve on top of the data points as a line
-    ax1.plot(x_vals, y_vals, label="Best Fit Curve")
 
     # Label the axes; change here
     ax1.set_xlabel("Time (s)")
     ax1.set_ylabel("Angle (rad)")
     ax1.set_title("Data & Best Fit Curve")
+    ax1.legend(loc="upper right")
 
-    # Print the values
-    print("Qty\tValue\t\t\tStdev")
-    print(f"A\t{a}\t{stdev_a}")
-    print(f"tau\t{tau}\t{stdev_tau}")
-    print(f"T\t{period}\t{stdev_period}")
-    print(f"phi\t{phi}\t{stdev_phi}")
+    if DO_FIT:
+        # Print the values
+        print("Qty\tValue\t\t\tStdev")
+        print(f"A\t{a}\t{stdev_a}")
+        print(f"tau\t{tau}\t{stdev_tau}")
+        print(f"T\t{period}\t{stdev_period}")
+        print(f"phi\t{phi}\t{stdev_phi}")
 
-    # Plot residuals
-    residuals = y_data - bestfit(x_data)
-    ax2.scatter(x_data, residuals, label="Residuals")
-    #ax2.errorbar(xdata, residual, yerr=yerror, xerr=xerror, fmt=".")
+        # Plot residuals
+        residuals = y_data - bestfit(x_data)
+        ax2.scatter(x_data, residuals, label="Residuals", s=4)
+        #ax2.errorbar(xdata, residual, yerr=yerror, xerr=xerror, fmt=".")
 
-    # Plot the zero line for reference
-    ax2.plot([start, stop], [0, 0])
+        # Plot the zero line for reference
+        ax2.plot([start, stop], [0, 0], "r")
 
-    ax2.set_xlabel("Time (s)")
-    ax2.set_ylabel("Fit Residual")
-    ax2.set_title("Fit Residuals")
+        ax2.set_xlabel("Time (s)")
+        ax2.set_ylabel("Fit Residual")
+        ax2.set_title("Fit Residuals")
 
     plt.show()
